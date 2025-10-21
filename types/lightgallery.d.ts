825f4321252b7f159c7e29ac019c7143cad69d2a
soportesrdigitalpro@gmail.com <kevinrobertotorresruiz@gmail.com>
2025-10-21 14:57:14 -0600

    Agregar declaraciones de tipos TypeScript para lightGallery

---
+++ types/lightgallery.d.ts
@@ -0,0 +1,36 @@
+declare module 'lg-thumbnail' {
+  const lgThumbnail: any;
+  export default lgThumbnail;
+}
+declare module 'lg-zoom' {
+  const lgZoom: any;
+  export default lgZoom;
+}
+declare module 'lg-autoplay' {
+  const lgAutoplay: any;
+  export default lgAutoplay;
+}
+declare module 'lg-fullscreen' {
+  const lgFullscreen: any;
+  export default lgFullscreen;
+}
+declare module 'lg-hash' {
+  const lgHash: any;
+  export default lgHash;
+}
+declare module 'lg-pager' {
+  const lgPager: any;
+  export default lgPager;
+}
+declare module 'lg-rotate' {
+  const lgRotate: any;
+  export default lgRotate;
+}
+declare module 'lg-share' {
+  const lgShare: any;
+  export default lgShare;
+}
+declare module 'lg-video' {
+  const lgVideo: any;
+  export default lgVideo;
+}
