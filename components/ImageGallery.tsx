f4ac4b8ede524c06302e72435eec129738cdf279
soportesrdigitalpro@gmail.com <kevinrobertotorresruiz@gmail.com>
2025-10-23 19:39:16 -0600

    cambios

--- components/ImageGallery.tsx
+++ components/ImageGallery.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
+import Image from 'next/image';
 
 interface ImageGalleryProps {
   images: string[];
@@ -80,10 +81,8 @@ export default function ImageGallery({ images, productName = 'Producto', classNa
     return (
       <div className={`bg-gray-200 rounded-lg flex items-center justify-center ${className}`}>
         <div className="text-gray-500 text-center p-4">
-          <svg className="w-12 h-12 mx-auto mb-2" fill="currentColor" viewBox="0 0 20 20">
-            <path fillRule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clipRule="evenodd" />
-          </svg>
-          <p>Sin imágenes disponibles</p>
+          <span className="text-4xl">📦</span>
+          <p className="text-xs mt-1">Sin imagen</p>
         </div>
       </div>
     );
